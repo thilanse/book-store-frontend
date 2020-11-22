14f717d2c8c9497bd1a576e5cbd0ca3d83c91b83
Thilan Senanayake <thilansenanayake@gmail.com>
2020-11-22 19:52:54 +0530

    Added username on header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,19 +4,26 @@ import {Link} from 'react-router-dom';
 
 class Header extends Component {
 
+    componentDidMount() {
+        console.log(this.props.username);
+    }
+
     render() {
 
         let loginLinks;
 
         if (localStorage.getItem("token") !== null)
         {
-            loginLinks = (
-                <ul className="navbar-nav col-auto mt-2 mt-lg-0 ml-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/" onClick={() => localStorage.clear()}>Logout</Link>
-                    </li>
-                </ul>
-            )
+                loginLinks = (
+                    <ul className="navbar-nav col-auto mt-2 mt-lg-0 ml-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">{this.props.username}</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/" onClick={() => localStorage.clear()}>Logout</Link>
+                        </li>
+                    </ul>
+                )
         }
         else
         {
